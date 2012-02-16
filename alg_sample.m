e6f8e0ebae623c6d89d4e64cc84f0dcf4fe3b15f
kenneth nwizege <kennethsorle@yahoo.com>
2012-02-16 16:34:57 +0000

    new changes in files and figure added

--- alg_sample.m
+++ alg_sample.m
@@ -68,7 +68,7 @@ end
 spd_set=v;
 % spd_set=0;
 
-       old_pos= rand(1,50)*1000;
+       old_pos= rand(1,10)*1000;
       Txnode = find(Mac.Bk_cnt==dt_temp);                % find the time of the first transmission attempt 
       Mac.Bk_cnt=Mac.Bk_cnt-dt_temp-1;                   % all backoff counters are decremented 
       Sim.time = Sim.time+ dt_temp*Sample.t_slot;       % update the simulation time accordingly
